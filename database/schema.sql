67d7f3006f431af90cd64a0dedb953c9ead1f55f
bernalas <bernalas@stthom.edu>
2023-03-02 20:11:15 -0600

    Added Work_For table

--- database/schema.sql
+++ database/schema.sql
@@ -17,11 +17,15 @@ CREATE TABLE "Employee" (
     "salary" INT NOT NULL CHECK (salary >= 0),
     "numberOfPackages" INT NOT NULL CHECK ('numberOfPackages' >= 0),
     "address" TEXT NOT NULL,
-    "startDate" DATE FOREIGN KEY NOT NULL,
+    "startDate" DATE NOT NULL,
     "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "createdBy" VARCHAR(30) NOT NULL,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedBy" VARCHAR(30) NOT NULL,
 
     CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Work_For" (
+    "hours" int NOT NULL
 );
