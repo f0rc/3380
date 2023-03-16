8ef7bdbbc6e20940c8caa19219d659d23191c959
f0rc <brickmp4@gmail.com>
2023-03-16 18:06:18 -0500

    basic sim

--- src/server/packageSimulator.ts
+++ src/server/packageSimulator.ts
@@ -1,94 +1,195 @@
-// import { postgresQuery } from "src/database/db";
-
-// // package simulations:
-// // 1. get packages every 10 seconds
-// // 2.update: {
-// //
-// // }
-
-// export const getPackage = async () => {
-//   // select packages 10 at a time where status is == accepted and in transit
-//   const getPackage = await postgresQuery(
-//     `select * from "Package_Location_History" where "status" = $1 OR "status" = $2 limit 10`,
-//     ["Accepted", "In Transit"]
-//   );
-//   console.log(getPackage.rows);
-// };
-
-export interface Package {
+export interface PackageList {
   id: string;
-  status: "intransit" | "accepted" | "delivered";
-  originZip: string;
-  destinationZip: string;
-  currentZip: string;
+  status: [
+    | "intransit"
+    | "accepted"
+    | "delivered"
+    | "out-for-delivery"
+    | "failed-delivery"
+  ];
+  zipcodeHistory: string[];
+  inTransitCounter: number | 0;
 }
-function simulatePackageDelivery(packages: Package[]) {
-  const zipCodes = ["10001", "60601", "80202", "94102", "90001"];
 
-  setInterval(() => {
-    packages.forEach((pkg) => {
-      if (pkg.status === "intransit") {
-        const currentZipIndex = zipCodes.indexOf(pkg.currentZip);
-        const destZipIndex = zipCodes.indexOf(pkg.destinationZip);
-
-        // Find the closest zip code to the current location
-        const distancesToCurrentZip = zipCodes.map((zip) => {
-          const distance = Math.abs(zipCodes.indexOf(zip) - currentZipIndex);
-          return { zip, distance };
-        });
-        const sortedByDistanceToCurrentZip = distancesToCurrentZip.sort(
-          (a, b) => a.distance - b.distance
-        );
-        const closestZipToCurrent = sortedByDistanceToCurrentZip[0].zip;
-
-        // Find the closest zip code to the destination
-        const distancesToDestZip = zipCodes.map((zip) => {
-          const distance = Math.abs(zipCodes.indexOf(zip) - destZipIndex);
-          return { zip, distance };
-        });
-        const sortedByDistanceToDestZip = distancesToDestZip.sort(
-          (a, b) => a.distance - b.distance
-        );
-        const closestZipToDest = sortedByDistanceToDestZip[0].zip;
-
-        // Pick the closest zip code that is also closer to the destination
-        const possibleNextZips = [closestZipToCurrent, closestZipToDest];
-        let nextZip = "";
-        for (const zip of possibleNextZips) {
-          if (zip !== pkg.currentZip) {
-            nextZip = zip;
-            break;
-          }
-        }
-        pkg.currentZip = nextZip;
-        console.log(`Package ${pkg.id} is now in zip code ${nextZip}.`);
-
-        // Check if the package has arrived at its destination
-        if (pkg.currentZip === pkg.destinationZip) {
-          pkg.status = "delivered";
-          console.log(
-            `Package ${pkg.id} has been delivered to zip code ${pkg.destinationZip}!`
-          );
-        }
-      }
-    });
-  }, 5000);
+interface PackageType {
+  pkgID: number;
+  id: number;
+  status: string;
+  currentZip: number;
+  intransitCounter: number;
 }
-const packages: Package[] = [
+
+const onePackage: PackageType[] = [
   {
-    id: "pkg001",
-    status: "intransit",
-    originZip: "10001",
-    destinationZip: "90001",
-    currentZip: "10001",
+    pkgID: 2,
+    id: 1,
+    status: "accepted",
+    currentZip: 100,
+    intransitCounter: 0,
   },
+  // {
+  //   pkgID: 2,
+  //   id: 2,
+  //   status: "intransit",
+  //   currentZip: 200,
+  //   intransitCounter: 1,
+  // },
+  // {
+  //   pkgID: 2,
+  //   id: 3,
+  //   status: "intransit",
+  //   currentZip: 623,
+  //   intransitCounter: 2,
+  // },
+  // {
+  //   pkgID: 2,
+  //   id: 4,
+  //   status: "intransit",
+  //   currentZip: 890,
+  //   intransitCounter: 3,
+  // },
+  // {
+  //   pkgID: 2,
+  //   id: 5,
+  //   status: "out-for-delivery",
+  //   currentZip: 1231,
+  //   intransitCounter: 3,
+  // },
+  // {
+  //   pkgID: 2,
+  //   id: 6,
+  //   status: "delivered",
+  //   currentZip: 91723,
+  //   intransitCounter: 3,
+  // },
+];
+
+const statusList = onePackage.map((pkg) => pkg.status);
+const zipcodelist = onePackage.map((pkg) => pkg.currentZip.toString());
+
+const packageList = [
   {
-    id: "pkg002",
-    status: "intransit",
-    originZip: "94102",
-    destinationZip: "60601",
-    currentZip: "80202",
+    id: onePackage[0]?.id,
+    status: statusList,
+    zipcodeHistory: zipcodelist,
+    inTransitCounter: onePackage[onePackage.length - 1]?.intransitCounter || 0,
   },
 ];
 
-simulatePackageDelivery(packages);
+console.log(packageList);
+
+const canTransit = (inTransitCounter: number) => {
+  if (inTransitCounter === 1) {
+    const change = Math.random() < 0.8;
+    console.log("CHANCE is 80%");
+    return change;
+  } else if (inTransitCounter === 2) {
+    const change = Math.random() < 0.6;
+    console.log("CHANCE is 60%");
+    return change;
+  } else if (inTransitCounter === 3) {
+    const change = Math.random() < 0.4;
+    console.log("CHANCE is 40%");
+    return change;
+  } else if (inTransitCounter === 4) {
+    const change = Math.random() < 0.2;
+    console.log("CHANCE is 20%");
+    return change;
+  }
+};
+
+const getRandomOfficeLocation = (zipcodeHistory: string[]): string => {
+  const postofficelocations = [
+    "10001",
+    "60601",
+    "80202",
+    "94102",
+    "90001",
+    "120312",
+    "23423",
+    "901723",
+    "788456",
+    "5695745",
+  ];
+  const randomLocation =
+    postofficelocations[Math.floor(Math.random() * postofficelocations.length)];
+  if (zipcodeHistory.includes(randomLocation!)) {
+    return getRandomOfficeLocation(zipcodeHistory);
+  }
+
+  return randomLocation as string;
+};
+
+function simulatePackageDelivery(packages: PackageType[]) {
+  const postofficelocations = ["10001", "60601", "80202", "94102", "90001"];
+  packageList.forEach((pkg) => {
+    const randomLocation =
+      postofficelocations[
+        Math.floor(Math.random() * postofficelocations.length)
+      ];
+
+    if (pkg.status[pkg.status.length - 1] === "delivered") {
+      return;
+    }
+    const isDeliveryFailed = Math.random() < 0.05;
+
+    if (
+      pkg.status[pkg.status.length - 1] === "intransit" &&
+      pkg.zipcodeHistory.length >= 2 &&
+      pkg.inTransitCounter >= 4
+    ) {
+      pkg.zipcodeHistory.push(randomLocation!);
+      pkg.status.push("out-for-delivery");
+      pkg.inTransitCounter = 0;
+    } else if (
+      pkg.status[pkg.status.length - 1] === "intransit" &&
+      pkg.zipcodeHistory.length >= 2 &&
+      pkg.inTransitCounter <= 4
+    ) {
+      if (canTransit(pkg.inTransitCounter)) {
+        pkg.zipcodeHistory.push(randomLocation!);
+        pkg.status.push("intransit");
+        pkg.inTransitCounter++;
+      } else {
+        pkg.zipcodeHistory.push(randomLocation!);
+        pkg.status.push("out-for-delivery");
+      }
+    } else if (pkg.status[pkg.status.length - 1] === "out-for-delivery") {
+      if (isDeliveryFailed) {
+        pkg.status.push("failed-delivery");
+        console.log(`Delivery failed for package ${pkg.id}`);
+      } else {
+        pkg.zipcodeHistory.push(randomLocation!);
+        pkg.status.push("delivered");
+        console.log(`Package ${pkg.id} delivered to`);
+      }
+    } else if (
+      pkg.status[pkg.status.length - 1] === "accepted" &&
+      pkg.zipcodeHistory.length === 1
+    ) {
+      pkg.zipcodeHistory.push(randomLocation!);
+      pkg.status.push("intransit");
+      pkg.inTransitCounter = 1;
+    } else if (pkg.status[pkg.status.length - 1] === "accepted") {
+      pkg.zipcodeHistory.push(randomLocation!);
+      pkg.status.push("intransit");
+      pkg.inTransitCounter = 1;
+    }
+  });
+}
+
+simulatePackageDelivery(onePackage);
+simulatePackageDelivery(onePackage);
+simulatePackageDelivery(onePackage);
+simulatePackageDelivery(onePackage);
+simulatePackageDelivery(onePackage);
+simulatePackageDelivery(onePackage);
+packageList.forEach((pkg) => {
+  console.log("++++++++++++++++++++++++++++++");
+  console.log("package id: ", pkg.id);
+  console.log("status: ", pkg.status);
+  console.log("zipcode history: ", pkg.zipcodeHistory);
+  console.log("transits: ", pkg.inTransitCounter);
+  console.log("++++++++++++++++++++++++++++++");
+});
+// }
