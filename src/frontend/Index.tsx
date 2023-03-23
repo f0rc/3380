2244c8a793eba69bcc55b578725b445e8b4f75d5
f0rc <brickmp4@gmail.com>
2023-03-23 16:06:50 -0500

    role based urls

--- src/frontend/Index.tsx
+++ src/frontend/Index.tsx
@@ -1,34 +1,11 @@
-import { useContext, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import { api } from "src/server/utils/api";
-import { AuthContext, AuthProvider } from "./auth/SessionProvider";
-import { CreateEmployee } from "./pages/CreateEmployee";
-import CreatePackage from "./pages/CreatePackage";
-import Error from "./pages/Error";
-import Home from "./pages/Home";
-import Layout from "./pages/Layout";
-import Login from "./pages/Login";
-import PackageDetail from "./pages/PackageDetail";
-import PackageList from "./pages/PackageList";
-import Signup from "./pages/Signup";
+import { AuthProvider } from "./auth/SessionProvider";
+import RoutesPage from "./RoutesPage";
 
 const Index = () => {
   return (
     <>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="signup" element={<Signup />} />
-            <Route path="login" element={<Login />} />
-            <Route path="create-package" element={<CreatePackage />} />
-            <Route path="package-list" element={<PackageList />} />
-            <Route path="/package/:id" element={<PackageDetail />} />
-            <Route path="create-employee" element={<CreateEmployee />} />
-
-            <Route path="*" element={<Error />} />
-          </Route>
-        </Routes>
+        <RoutesPage />
       </AuthProvider>
     </>
   );
