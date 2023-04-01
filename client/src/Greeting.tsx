cb8f183f0f14ea2346ead4a3590426ec28c07d32
f0rc <brickmp4@gmail.com>
2023-03-31 22:51:42 -0500

    tables for products process

--- client/src/Greeting.tsx
+++
@@ -1,7 +0,0 @@
-import { trpc } from "./utils/trpc";
-
-export function Greeting() {
-  const greeting = trpc.greeting.hello.useQuery();
-
-  return <div>hello {greeting.data?.greeting}</div>;
-}
--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
   // CORS change this to your domain
   res.setHeader("Access-Control-Allow-Origin", `${process.env.PUBLIC_URL}`);
   res.setHeader("Access-Control-Request-Method", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
   res.setHeader("Access-Control-Allow-Headers", "content-type");
   res.setHeader("Referrer-Policy", "no-referrer");
   res.setHeader("Access-Control-Allow-Credentials", "true");
