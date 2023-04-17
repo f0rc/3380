42fe21fca8ca1896bfa9162385b21e1b3b6be0b3
mopscr <brickmp4@gmail.com>
2023-04-17 15:15:36 -0500

    work log sort fix, product report init

--- server/database/test.ts
+++ server/database/test.ts
@@ -1,39 +1,63 @@
 import { postgresQuery } from "./db";
 
 const createAdmin = async () => {
-  const getEmployeeInfo = await postgresQuery(
-    `SELECT
-    E.employee_id,
-    E.firstname,
-    E.lastname,
-    E.email,
-    E.birthdate,
-    E.role,
-    E.salary,
-    E.manager_id,
-    E.address_street,
-    E.address_city,
-    E.address_state,
-    E.address_zipcode,
-    E.startdate,
-    E."createdAt",
-    E."createdBy",
-    E."updatedAt",
-    E."updatedBy",
-    PL.locationname AS "work_location",
-    SUM(WL.hours) AS "total_hours"
-    FROM
-        "EMPLOYEE" AS E
-    JOIN "WORKS_FOR" AS WF ON E.employee_id = WF.employee_id
-    JOIN "POSTOFFICE_LOCATION" AS PL ON WF.postoffice_location_id = PL.postoffice_location_id
-    JOIN "WORK_LOG" AS WL ON E.employee_id = WL.employee_id
-    GROUP BY
-    E.employee_id,
-    PL.locationname;`,
-    []
+  // const getEmployeeInfo = await postgresQuery(``, []);
+  // const getOrders = await postgresQuery(
+  //   `WITH orders_filtered AS (
+  //   SELECT
+  //       o.order_id,
+  //       o.postoffice_location_id,
+  //       o.order_date
+  //   FROM
+  //       "ORDER" AS o
+  //   JOIN
+  //       "POSTOFFICE_LOCATION" AS pl
+  //   ON
+  //       o.postoffice_location_id = pl.postoffice_location_id
+  //   WHERE
+  //       TRUE AND pl.postoffice_location_id = $1
+  //       ) SELECT
+  //       of.order_id,
+  //       of.postoffice_location_id,
+  //       pl.locationname,
+  //       pl.address_street,
+  //       pl.address_city,
+  //       pl.address_state,
+  //       pl.address_zipcode,
+  //       of.order_date,
+  //       oi.product_id,
+  //     p.product_name,
+  //     oi.quantity,
+  //     oi.price
+  //     FROM
+  //     orders_filtered AS of
+  //   JOIN
+  //     "POSTOFFICE_LOCATION" AS pl
+  //   ON
+  //     of.postoffice_location_id = pl.postoffice_location_id
+  //   JOIN
+  //     "ORDER_ITEMS" AS oi
+  //   ON
+  //     of.order_id = oi.order_id
+  //   JOIN
+  //     "PRODUCT" AS p
+  //   ON
+  //     oi.product_id = p.product_id
+  //   ORDER BY
+  //     of.order_date;
+  //       `,
+  //   ["61e57254-20db-4443-90a0-864505c17cbf"]
+  // );
+  // console.log(getOrders.rows);
+
+  const orderReport = await postgresQuery(
+    `
+      
+      `,
+    ["61e57254-20db-4443-90a0-864505c17cbf"]
   );
 
-  console.log(getEmployeeInfo.rows);
+  console.log(orderReport.rows);
 };
 
 createAdmin();
